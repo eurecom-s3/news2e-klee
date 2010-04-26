cb89f8eeb704309b008542add8f3b968caed8822
Volodymyr Kuznetsov <vova.kuznetsov@epfl.ch>
2010-04-26 19:55:26 +0000

    Added AddressSpace::resolveOne method with concrete address
    git-svn-id: https://dslabpc10.epfl.ch/svn/src/s2e/trunk@9271 f94e2f5b-dfd8-4992-a279-46519bc7c46e

--- lib/Core/AddressSpace.cpp
+++ lib/Core/AddressSpace.cpp
@@ -51,6 +51,22 @@ ObjectState *AddressSpace::getWriteable(const MemoryObject *mo,
 
 /// 
 
+bool AddressSpace::resolveOne(uint64_t address,
+                              ObjectPair &result) {
+  MemoryObject hack(address);
+
+  if (const MemoryMap::value_type *res = objects.lookup_previous(&hack)) {
+    const MemoryObject *mo = res->first;
+    if ((mo->size==0 && address==mo->address) ||
+        (address - mo->address < mo->size)) {
+      result = *res;
+      return true;
+    }
+  }
+
+  return false;
+}
+
 bool AddressSpace::resolveOne(const ref<ConstantExpr> &addr, 
                               ObjectPair &result) {
   uint64_t address = addr->getZExtValue();
