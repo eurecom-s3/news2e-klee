032dac941e1a439b9731442401ff9234d03b11e2
Jonas Zaddach <zaddach@eurecom.fr>
2016-06-24 14:49:36 +0000

    Added more C interfaces for ObjectState

--- include/klee-c/klee.h
+++ include/klee-c/klee.h
@@ -20,7 +20,34 @@ using klee::ObjectState;
 typedef struct ObjectState ObjectState;
 #endif /* defined(__cplusplus) */
 
+/**
+ * Verify if a given range is concrete.
+ * @param self The object.
+ * @param offset Byte offset of the start.
+ * @param bit_size Size of the range in bits.
+ * @return <b>true</b> if the range is entirely concrete, <b>false</b> otherwise.
+ */
 bool klee_ObjectState_IsConcrete(ObjectState* os, unsigned offset, unsigned bit_size);
+
+/**
+ * This function reads concrete data from the ObjectState.
+ * @param self The object.
+ * @param offset Byte offset of the data.
+ * @param out Pointer where read data is written to.
+ * @param size Byte size of the data.
+ * @return <b>true</b> if data was read successfully (all data is concrete). <b>false</b> otherwise.
+ */
+bool klee_ObjectState_ReadConcrete(ObjectState* self, unsigned offset, uint8_t* out, unsigned size);
+
+/**
+ * This function writes concrete data to the ObjectState.
+ * @param self The object.
+ * @param offset Byte offset of the data in the ObjectState.
+ * @param data Pointer to the data.
+ * @param size Byte size of the data buffer.
+ */
+
+void klee_ObjectState_WriteConcrete(ObjectState* self, unsigned offset, const uint8_t* data, unsigned size);
 
 
 #if defined(__cplusplus)
