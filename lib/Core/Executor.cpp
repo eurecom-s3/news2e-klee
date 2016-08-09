b9adc907446e5ec906132d7267c2291a4664843d
Jonas Zaddach <zaddach@eurecom.fr>
2016-08-09 12:54:09 +0000

    Initialize concolicMode variale in Executor

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -336,7 +336,7 @@ Executor::Executor(const InterpreterOptions &opts, InterpreterHandler *ih, Execu
     : Interpreter(opts), kmodule(0), interpreterHandler(ih), searcher(0),
       externalDispatcher(new ExternalDispatcher(engine)), solver(NULL), statsTracker(0),
       pathWriter(0), symPathWriter(0), specialFunctionHandler(0),
-      processTree(0), replayKTest(0), replayPath(0), usingSeeds(0),
+      processTree(0), concolicMode(false), replayKTest(0), replayPath(0), usingSeeds(0),
       atMemoryLimit(false), inhibitForking(false), haltExecution(false),
       ivcEnabled(false),
       coreSolverTimeout(MaxCoreSolverTime != 0 && MaxInstructionTime != 0
